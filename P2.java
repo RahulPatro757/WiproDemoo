3ffa08f6acd5547e6bbb7f62396f1c7ebb6a80dc
RahulPatro757 <rj808355@gmail.com>
2025-01-28 11:05:35 +0530

    Multiplication and Modulo Code

---
+++ P2.java
@@ -0,0 +1,15 @@
+public class P2 {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 10;
+        int num2 = 3;
+
+        // Perform multiplication
+        int multiplicationResult = num1 * num2;
+        System.out.println("Multiplication Result: " + multiplicationResult);
+
+        // Perform modulo operation
+        int moduloResult = num1 % num2;
+        System.out.println("Modulo Result: " + moduloResult);
+    }
+}
